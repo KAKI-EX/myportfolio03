286b620c1facb799e5c8b059705ee5bf57a26364
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2023-04-25 21:22:57 +0900

    ■ 概要: devise_token_authを導入したもののdowncaseエラーが発生。devise_token_auth.rbを修正することで解決。一旦コミット実施。

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000'
+    origins "localhost:4000" # React側はポート番号3000で作るので「localhost:3000」を指定
 
-    resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
+    resource "*",
+      headers: :any,
+      expose: ["access-token", "expiry", "token-type", "uid", "client"], # 追記
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
