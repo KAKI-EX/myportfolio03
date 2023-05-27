caf2b5bc6cbfbb5f2ce752ac0a29ed700ec9bf4b
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2023-05-27 21:21:05 +0900

    ■ 概要: OkaimonoMemo作成ページに、「買い物をしながらメモを作るユーザー」(事前にメモ入力をせずに、買い物の記録用として使用するユーザー)を想定し、消費期限入力フォームを追加していく。事前準備として既存フォームを調整。（VStackからHStackへ変更）  Changes to be committed:        modified:   src/components/molecules/OkaimonoDetail.tsx

--- frontend/src/components/molecules/OkaimonoDetail.tsx
+++ frontend/src/components/molecules/OkaimonoDetail.tsx
@@ -91,91 +91,95 @@ export const OkaimonoDetail: VFC<Props> = memo((props) => {
               />
             </Box>
           </VStack>
-          <VStack w="60%">
-            <Box w="100%">
-              <Input
-                isReadOnly={readOnly}
-                bg={readOnly ? "blackAlpha.200" : "white"}
-                autoFocus={false}
-                placeholder={!readOnly ? "買う商品のなまえ" : ""}
-                fontSize={{ base: "sm", md: "md" }}
-                size="md"
-                w="100%"
-                {...register(`listForm.${index}.purchase_name`, {
-                  required: { value: true, message: "商品名が入力されていません" },
-                  maxLength: { value: 30, message: "最大文字数は30文字までです。" },
-                })}
-              />
-              {errors.listForm && errors.listForm[index]?.purchase_name && (
-                <Box color="red" fontSize="sm">
-                  {errors.listForm[index]?.purchase_name?.types?.required}
-                  {errors.listForm[index]?.purchase_name?.types?.maxLength}
-                </Box>
-              )}
-            </Box>
-            <Box w="100%">
-              <Input
-                isReadOnly={readOnly}
-                bg={readOnly ? "blackAlpha.200" : "white"}
-                placeholder={!readOnly ? "メモ" : ""}
-                fontSize={{ base: "sm", md: "md" }}
-                size="md"
-                {...register(`listForm.${index}.shopping_detail_memo`, {
-                  maxLength: { value: 150, message: "最大文字数は150文字です。" },
-                })}
-              />
-              {errors.listForm && errors.listForm[index]?.shopping_detail_memo && (
-                <Box color="red" fontSize="sm">
-                  {errors.listForm[index]?.shopping_detail_memo?.types?.maxLength}
-                </Box>
-              )}
-            </Box>
-          </VStack>
-          <VStack w="30%">
-            <Input
-              isReadOnly={readOnly}
-              bg={readOnly ? "blackAlpha.200" : "white"}
-              placeholder={!readOnly ? "個数" : ""}
-              fontSize={{ base: "sm", md: "md" }}
-              size="md"
-              w="100%"
-              type="number"
-              min="1"
-              {...register(`listForm.${index}.amount`, {
-                max: { value: 99, message: "上限は99までです。" },
-                pattern: { value: validationNumber, message: "半角整数で入力してください。" },
-              })}
-            />
-            {errors.listForm && errors.listForm[index]?.amount && (
-              <Box color="red" fontSize="sm">
-                {errors.listForm[index]?.amount?.types?.max}
-                {errors.listForm[index]?.amount?.types?.pattern}
-              </Box>
-            )}
-            <Box w="100%">
-              <InputGroup>
+          <VStack>
+            <HStack w="100%">
+              <Box w="70%">
                 <Input
                   isReadOnly={readOnly}
                   bg={readOnly ? "blackAlpha.200" : "white"}
-                  placeholder={!readOnly ? "いくら？" : ""}
-                  type="number"
+                  autoFocus={false}
+                  placeholder={!readOnly ? "買う商品のなまえ" : ""}
                   fontSize={{ base: "sm", md: "md" }}
-                  {...register(`listForm.${index}.price`, {
+                  size="md"
+                  w="100%"
+                  {...register(`listForm.${index}.purchase_name`, {
+                    required: { value: true, message: "商品名が入力されていません" },
+                    maxLength: { value: 30, message: "最大文字数は30文字までです。" },
+                  })}
+                />
+                {errors.listForm && errors.listForm[index]?.purchase_name && (
+                  <Box color="red" fontSize="sm">
+                    {errors.listForm[index]?.purchase_name?.types?.required}
+                    {errors.listForm[index]?.purchase_name?.types?.maxLength}
+                  </Box>
+                )}
+              </Box>
+              <Box w="30%">
+                <Input
+                  isReadOnly={readOnly}
+                  bg={readOnly ? "blackAlpha.200" : "white"}
+                  placeholder={!readOnly ? "個数" : ""}
+                  fontSize={{ base: "sm", md: "md" }}
+                  size="md"
+                  w="100%"
+                  type="number"
+                  min="1"
+                  {...register(`listForm.${index}.amount`, {
+                    max: { value: 99, message: "上限は99までです。" },
                     pattern: { value: validationNumber, message: "半角整数で入力してください。" },
                   })}
                 />
-                <InputRightElement pointerEvents="none" color="gray.300" fontSize={{ base: "sm", md: "md" }}>
-                  円
-                </InputRightElement>
-              </InputGroup>
-            </Box>
-            {errors.listForm && errors.listForm[index]?.price && (
-              <Box color="red" fontSize="sm">
-                {errors.listForm[index]?.price?.types?.pattern}
+                {errors.listForm && errors.listForm[index]?.amount && (
+                  <Box color="red" fontSize="sm">
+                    {errors.listForm[index]?.amount?.types?.max}
+                    {errors.listForm[index]?.amount?.types?.pattern}
+                  </Box>
+                )}
               </Box>
-            )}
-            <Input type="hidden" {...register(`listForm.${index}.id`)} />
-            <Input type="hidden" {...register(`listForm.${index}.asc`)} />
+            </HStack>
+            <HStack w="100%">
+              <Box w="70%">
+                <Input
+                  isReadOnly={readOnly}
+                  bg={readOnly ? "blackAlpha.200" : "white"}
+                  placeholder={!readOnly ? "メモ" : ""}
+                  fontSize={{ base: "sm", md: "md" }}
+                  size="md"
+                  {...register(`listForm.${index}.shopping_detail_memo`, {
+                    maxLength: { value: 150, message: "最大文字数は150文字です。" },
+                  })}
+                />
+                {errors.listForm && errors.listForm[index]?.shopping_detail_memo && (
+                  <Box color="red" fontSize="sm">
+                    {errors.listForm[index]?.shopping_detail_memo?.types?.maxLength}
+                  </Box>
+                )}
+              </Box>
+              <Box w="30%">
+                <InputGroup>
+                  <Input
+                    isReadOnly={readOnly}
+                    bg={readOnly ? "blackAlpha.200" : "white"}
+                    placeholder={!readOnly ? "いくら？" : ""}
+                    type="number"
+                    fontSize={{ base: "sm", md: "md" }}
+                    {...register(`listForm.${index}.price`, {
+                      pattern: { value: validationNumber, message: "半角整数で入力してください。" },
+                    })}
+                  />
+                  <InputRightElement pointerEvents="none" color="gray.300" fontSize={{ base: "sm", md: "md" }}>
+                    円
+                  </InputRightElement>
+                </InputGroup>
+              </Box>
+              {errors.listForm && errors.listForm[index]?.price && (
+                <Box color="red" fontSize="sm">
+                  {errors.listForm[index]?.price?.types?.pattern}
+                </Box>
+              )}
+              <Input type="hidden" {...register(`listForm.${index}.id`)} />
+              <Input type="hidden" {...register(`listForm.${index}.asc`)} />
+            </HStack>
           </VStack>
         </HStack>
       ))}
