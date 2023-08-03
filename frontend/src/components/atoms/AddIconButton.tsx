eee789f33b0b43c9e603f40270c5f98c3251df62
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2023-08-03 17:56:14 +0900

    ■ 概要: テストするコンポーネント「OkaimonoDetail」を更にコンポーネント分割。現在は規模が大きすぎる。  Changes to be committed:        new file:   src/components/atoms/AddIconButton.tsx

---
+++ frontend/src/components/atoms/AddIconButton.tsx
@@ -0,0 +1,28 @@
+import { SmallAddIcon } from "@chakra-ui/icons";
+import { memo, VFC } from "react";
+
+type Props = {
+  readOnly?: boolean;
+  insertInputForm: (index: number) => void;
+  index: number;
+};
+
+export const AddIconButton: VFC<Props> = memo((props) => {
+  const { readOnly, insertInputForm, index } = props;
+  return (
+    <SmallAddIcon
+      bg="teal.500"
+      rounded="full"
+      color="white"
+      onClick={(event) => {
+        if (readOnly) {
+          event.preventDefault();
+          // eslint-disable-next-line no-alert
+          alert("確認画面では使用できません。");
+          return;
+        }
+        insertInputForm(index);
+      }}
+    />
+  );
+});
